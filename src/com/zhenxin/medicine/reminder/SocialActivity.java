b5b42a3205d51019ecdaadcec967004baa489da9
Ivan <ivanpeng13@gmail.com>
2013-06-15 01:23:41 +0800

    Added camera functionality back. Added a selection interface for SocialActivity.

--- src/com/zhenxin/medicine/reminder/SocialActivity.java
+++ src/com/zhenxin/medicine/reminder/SocialActivity.java
@@ -3,19 +3,26 @@ package com.zhenxin.medicine.reminder;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.ContentResolver;
-import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Intents.Insert;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
  
 public class SocialActivity extends ListActivity {
+	
+	final static int PICK_CONTACT = 999;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.social_layout);
@@ -62,6 +69,42 @@ public class SocialActivity extends ListActivity {
  
     	return adapter;
     }
+    
+    /**
+     * This method simply creates a dialog for display to ask if you want to view the contact
+     * or register for notifications
+     * TODO: add functionality for notification registration
+     */
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		final String[] options = {"View contact information", "Register contact for missed SMS notifications"};
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Contact Options");
+		builder.setItems(options, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// This dialog will interpret which option is selected, and then implement
+				// the choice selected
+				if (which == 0)	{
+					// View contact; do this by building intent, and then starting the intent
+					 Intent intent = new Intent(Intent.ACTION_VIEW);
+                     intent.setType(ContactsContract.Contacts.CONTENT_ITEM_TYPE);
+                     //TODO: fix
+                     startActivity(intent);
+				} else {
+					// Register contact
+					Toast.makeText(getApplicationContext(), "Contact has been registered for SMS notification in case of missed medication!", Toast.LENGTH_LONG).show();
+				}
+				
+			}
+			
+
+		});
+		builder.show();
+	}
+	
+	
     
     
 }
